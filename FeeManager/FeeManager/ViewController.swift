b9ef7a1e7b32457501592d06971ed4ae4e1f778a
Milovan <marian.arsul00@e-uvt.ro>
2023-01-12 15:20:46 +0200

    Project creation

---
+++ FeeManager/FeeManager/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeeManager
+//
+//  Created by Milovan Arsul on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
